d4330fd52a054f73166fc518b9e3edd24e5fc24b
Zaid-EL-IDRISSI <109754259+Zaid-EL-IDRISSI@users.noreply.github.com>
2025-09-13 15:24:22 +0100

    Add files via upload

---
+++ qcmfolder/script.js
@@ -0,0 +1,246 @@
+let as2ila = 
+[{ 
+    "question":"Quel est l'origine du volley-ball ?",
+    "repons1":"Il a été inventé en Europe au 19e siècle.",
+    "repons2":"Il a émergé en Afrique dans les années 1920.",
+    "repons3":"Il a été inventé en Asie au début du 20e siècle.",
+    "repons4":" Il a été inventé aux États-Unis à la fin du 19e siècle par William G. Morgan.",
+     "bonreponse":" Il a été inventé aux États-Unis à la fin du 19e siècle par William G. Morgan."
+    },
+    { 
+    "question":"Quels éléments sont inclus dans les techniques de la course de vitesse ?",
+    "repons1":"La météo et les conditions atmosphériques.",
+    "repons2":"La nutrition et l'hydratation.",
+    "repons3":"La technique de départ, la posture du corps, la cadence des foulées, l'utilisation des bras et des jambes, ainsi que des stratégies pour maintenir la vitesse.",
+    "repons4":"La planification de l'entraînement et la récupération musculaire.",
+    "bonreponse":"La technique de départ, la posture du corps, la cadence des foulées, l'utilisation des bras et des jambes, ainsi que des stratégies pour maintenir la vitesse."
+    },
+    { 
+    "question":"Quelle est la recommandation pour assurer une récupération adéquate et gérer les douleurs ou blessures persistantes en course de vitesse ?",
+    "repons1":"Continuer à s'entraîner intensément malgré la douleur.",
+    "repons2":"Utiliser des médicaments anti-inflammatoires sans prescription pour atténuer la douleur.",
+    "repons3":"Prendre des jours de repos uniquement en cas de blessure grave.",
+    "repons4":"Accorder suffisamment de temps pour récupérer entre les séances d'entraînement et consulter un professionnel de la santé en cas de douleur persistante ou de blessure.",
+    "bonreponse":"Accorder suffisamment de temps pour récupérer entre les séances d'entraînement et consulter un professionnel de la santé en cas de douleur persistante ou de blessure."
+    },
+    { 
+    "question":"Comment le volley-ball a-t-il gagné en popularité mondiale ?",
+    "repons1":"Grâce à des campagnes de marketing agressives dans les années 1950.",
+    "repons2":"Par la création de la Fédération Internationale de Volley-ball (FIVB) en 1947.",
+    "repons3":"Suite à sa reconnaissance par la FIFA en 1947.",
+    "repons4":"Après sa présentation aux Jeux Olympiques de 1948.",
+    "bonreponse":"Par la création de la Fédération Internationale de Volley-ball (FIVB) en 1947."
+    },
+    { 
+    "question":"Quels sont les éléments essentiels pour maintenir des performances optimales en course de vitesse ?",
+    "repons1":"Aliments riches en matières grasses et en sucre.",
+    "repons2":"Aliments riches en glucides et en protéines, ainsi qu'une hydratation adéquate.",
+    "repons3":"Aliments riches en fibres et en sodium.",
+    "repons4":"Aliments riches en vitamines et minéraux, et une hydratation limitée.",
+    "bonreponse":"Aliments riches en glucides et en protéines, ainsi qu'une hydratation adéquate."
+    },
+    { 
+    "question":"Quel est le cycle de l'événement Coupe du Monde de Volleyball ?",
+    "repons1":"Tous les deux ans.",
+    "repons2":"Tous les quatre ans.",
+    "repons3":"Tous les six ans.",
+    "repons4":"Tous les huit ans.",
+    "bonreponse":"Tous les quatre ans."
+    },
+    { 
+    "question":"Quels sont les éléments essentiels pour jouer confortablement et en toute sécurité au volley-ball ?",
+    "repons1":"Ballon de volley-ball et raquette.",
+    "repons2":"Filet et poteaux de filet.",
+    "repons3":"Chaussures de volley-ball et casque.",
+    "repons4":" Tenue de jeu et protections corporelles.",
+    "bonreponse":"Filet et poteaux de filet."
+    },
+    { 
+    "question":"Quelles mesures sont essentielles pour prévenir les blessures chez les gymnastes ?",
+    "repons1":"La consommation de suppléments nutritionnels.",
+    "repons2":"La pratique régulière de la méditation.",
+    "repons3":"Des échauffements, des étirements et une technique correcte.",
+    "repons4":"L'utilisation de vêtements de protection robustes.",
+    "bonreponse":"Des échauffements, des étirements et une technique correcte."
+    },
+    { 
+    "question":"Combien de joueurs sont généralement sur le terrain pour chaque équipe de volley-ball ?",
+    "repons1":"Quatre joueurs.",
+    "repons2":"Dix joueurs.",
+    "repons3":"Huit joueurs.",
+    "repons4":"Six joueurs.",
+    "bonreponse":"Six joueurs."
+    },
+    { 
+    "question":"Quelle pratique est essentielle pour prévenir les blessures courantes en course de vitesse ?",
+    "repons1":"L'application de glace sur les muscles après l'entraînement.",
+    "repons2":"L'utilisation de techniques de relaxation musculaire profonde.",
+    "repons3":"Un échauffement approprié avant chaque séance, en accordant une attention particulière aux muscles et aux articulations sollicités.",
+    "repons4":"La prise de suppléments de calcium et de magnésium.",
+    "bonreponse":"Un échauffement approprié avant chaque séance, en accordant une attention particulière aux muscles et aux articulations sollicités."
+    },
+    { 
+    "question":"Quelle est la première action qui débute un match de volley-ball ?",
+    "repons1":"La passe.",
+    "repons2":"Le service.",
+    "repons3":"Le blocage.",
+    "repons4":"La réception.",
+    "bonreponse":"Le service."
+    },
+    { 
+    "question":"Quelles caractéristiques décrivent la gymnastique ?",
+    "repons1":"Souplesse et force musculaire.",
+    "repons2":"Agilité et endurance.",
+    "repons3":"Force et vitesse.",
+    "repons4":"Endurance et coordination.",
+    "bonreponse":"Souplesse et force musculaire."
+    },
+    { 
+    "question":"Quel est l'évolution historique de la gymnastique ?",
+    "repons1":"De la navigation maritime à une activité récréative.",
+    "repons2":"De l'entraînement militaire à un sport artistique et compétitif.",
+    "repons3":"De la danse folklorique à un sport de combat.",
+    "repons4":"De l'agriculture à un sport olympique.",
+    "bonreponse":"De l'entraînement militaire à un sport artistique et compétitif."
+    },
+    { 
+    "question":"Quels éléments sont considérés comme importants dans le monde de la gymnastique ?",
+    "repons1":"Des justaucorps ajustés et des équipements tels que les barres asymétriques, la poutre et le sol.",
+    "repons2":"Des chaussures de course et des leggings.",
+    "repons3":"Des gants de boxe et des haltères.",
+    "repons4":"Des maillots de bain et des raquettes de tennis.",
+    "bonreponse":"Des justaucorps ajustés et des équipements tels que les barres asymétriques, la poutre et le sol."
+    },
+    { 
+    "question":"Quel est l'objectif principal de la course de vitesse ?",
+    "repons1":"Développer la flexibilité musculaire.",
+    "repons2":"Améliorer la coordination œil-main.",
+    "repons3":"Parcourir une distance en un temps minimal.",
+    "repons4":"Augmenter la capacité aérobie.",
+    "bonreponse":"Parcourir une distance en un temps minimal."
+    }
+]
+let index = 0;
+let stain = 0;
+let inputtselection=0;
+let jawablmokhtar;
+let ljawabsa7i7;
+let score = 0;
+let statistique = [
+    {"resultquestion":""},
+    {"resultquestion":""},
+    {"resultquestion":""},
+    {"resultquestion":""},
+    {"resultquestion":""},
+    {"resultquestion":""},
+    {"resultquestion":""},
+    {"resultquestion":""},
+    {"resultquestion":""},
+    {"resultquestion":""},
+    {"resultquestion":""},
+    {"resultquestion":""},
+    {"resultquestion":""},
+    {"resultquestion":""},
+    {"resultquestion":""}
+]
+let button = document.getElementById("botona");
+let buttontitle = document.getElementById("verifierbutton");
+let listsat = document.getElementById("liststatistique");
+let so2aldyali =  document.getElementById("titleso2al");
+let jawab1 = document.getElementById("repons1h3");
+let jawab2 = document.getElementById("repons2h3");
+let jawab3 = document.getElementById("repons3h3");
+let jawab4 = document.getElementById("repons4h3");
+let scooor = document.getElementById("scor");
+console.log(button);
+if (index === 0){
+    so2aldyali.appendChild(document.createTextNode((as2ila[0]).question))
+    jawab1.appendChild(document.createTextNode((as2ila[0]).repons1))
+    jawab2.appendChild(document.createTextNode((as2ila[0]).repons2))
+    jawab3.appendChild(document.createTextNode((as2ila[0]).repons3))
+    jawab4.appendChild(document.createTextNode((as2ila[0]).repons4))
+
+}
+document.getElementById("liststatistique").style.visibility = "hidden"
+button.onclick = function(){
+
+
+
+    if(inputtselection !== 0){
+        switch(inputtselection){
+            case 1:jawablmokhtar = as2ila[index].repons1;
+                break;
+            case 2:jawablmokhtar = as2ila[index].repons2;
+                break;
+            case 3:jawablmokhtar = as2ila[index].repons3;
+                break;
+            case 4:jawablmokhtar = as2ila[index].repons4;
+                break;
+        }
+       
+        ljawabsa7i7 = as2ila[index].bonreponse;
+        
+        if (jawablmokhtar == ljawabsa7i7){
+            score = score + 1;
+            statistique[index].resultquestion = "Votre réponse est correct"
+            jawablmokhtar = "";
+        }
+        else{
+            statistique[index].resultquestion = "Votre réponse est incorrect, la bonne réponse est:" + "(" + as2ila[index].bonreponse + ")"
+            jawablmokhtar = "";
+        }
+        
+            
+        console.log(as2ila.length)
+        if(index < as2ila.length - 1){index++;}else{console.log(statistique,"score:",score)}
+        console.log(index)
+        jawab1.innerHTML = "";
+        jawab2.innerHTML = "";
+        jawab3.innerHTML = "";
+        jawab4.innerHTML = "";
+        jawab1.appendChild(document.createTextNode((as2ila[index]).repons1));
+        jawab2.appendChild(document.createTextNode((as2ila[index]).repons2));
+        jawab3.appendChild(document.createTextNode((as2ila[index]).repons3));
+        jawab4.appendChild(document.createTextNode((as2ila[index]).repons4));
+        so2aldyali.innerHTML = "";
+        so2aldyali.appendChild(document.createTextNode((as2ila[index]).question));
+        if(stain == 11){
+            console.log("KHDAAAAAM STATISTIQUE")
+            button.style.display = "none";
+            document.getElementById("divq1").style.display = "none";
+            document.getElementById("divq2").style.display = "none";
+            document.getElementById("divq3").style.display = "none";
+            document.getElementById("divq4").style.display = "none";
+            so2aldyali.style.display = "none";
+            scooor.appendChild(document.createTextNode("Votre score :" + ((score*100)/as2ila.length).toString().slice(0,3) + "%"))
+            if(score <= 5){scooor.style.color = "red";}else{if(score <= 10){scooor.style.color = "yellow";}else{if(score <= 15){scooor.style.color = "green";}}}
+            for(let r = 0; r < 15;r++){
+            list = document.createElement("li");
+            brr = document.createElement("br");
+            list.appendChild(document.createTextNode(as2ila[r].question + statistique[r].resultquestion))
+            listsat.appendChild(list)
+            listsat.appendChild(brr)
+            }
+	document.getElementById("liststatistique").style.visibility = "visible";
+        }
+        if(index == as2ila.length -1){
+            buttontitle.innerHTML = "";
+            buttontitle.appendChild(document.createTextNode("Vérifier"))
+            stain = 11;
+        }
+        
+    }
+
+}
+function input1oncklick(){
+    inputtselection=1;
+}
+function input2oncklick(){
+    inputtselection=2;
+}
+function input3oncklick(){
+    inputtselection=3;
+}
+function input4oncklick(){
+    inputtselection=4;
+}
